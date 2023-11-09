9672349edbf4131f4793358c82569226a4447b1a
João Neto <67469792+J0NGS@users.noreply.github.com>
2023-11-09 15:54:44 -0300

    feat/multithreading, add multhithreading processor in main

--- draenceia/src/main/java/com/jongs/br/Main.java
+++ draenceia/src/main/java/com/jongs/br/Main.java
@@ -4,42 +4,66 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class Main {
+
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        String source1 = "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/1.png";
-        String source2 = "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/2.png";
-        String source3 = "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/3.png";
-        String source4 = "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/4.png";
+        String[] imageSources = {
+                "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/1.png",
+                "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/2.png",
+                "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/3.png",
+                "draenceia/src/main/resources/tesseractDraence/jogador/Nabi/4.png"
+        };
 
-        Reader reader = new Reader();
-        List<String> list1 = reader.readHabilityOverral(source1, 1);
-        List<String> list2 = reader.readHabilityOverral(source2, 2);
-        List<String> list3 = reader.readHabilityOverral(source3, 3);
-        List<String> list4 = reader.readHabilityOverral(source4, 4);
+        ExecutorService executorService = Executors.newFixedThreadPool(imageSources.length);
+        List<Future<List<String>>> futures = new ArrayList<>();
 
-        Player nabi = new Player(list1, list2, list3, list4);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String nabiStr = gson.toJson(nabi);
-        System.out.println(nabiStr);
+        try {
+            for (int i = 0; i < imageSources.length; i++) {
+                final int page = i + 1;
+                String source = imageSources[i];
 
-        try (FileOutputStream fos = new FileOutputStream("draenceia/src/main/resources/tesseractDraence/jogador/Nabi/Nabi.json");
-             OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8)) {
-            osw.write(nabiStr);
-            System.out.println("JSON salvo com sucesso no arquivo Nabi.json");
-        } catch (IOException e) {
+                futures.add(executorService.submit(() -> {
+                    Reader reader = new Reader();
+                    return reader.readHabilityOverral(source, page);
+                }));
+            }
+
+            List<String> list1 = futures.get(0).get();
+            List<String> list2 = futures.get(1).get();
+            List<String> list3 = futures.get(2).get();
+            List<String> list4 = futures.get(3).get();
+
+            Player nabi = new Player(list1, list2, list3, list4);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String nabiStr = gson.toJson(nabi);
+            System.out.println(nabiStr);
+
+            try (FileOutputStream fos = new FileOutputStream("draenceia/src/main/resources/tesseractDraence/jogador/Nabi/Nabi.json");
+                 OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8)) {
+                osw.write(nabiStr);
+                System.out.println("JSON salvo com sucesso no arquivo Nabi.json");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            executorService.shutdown();
         }
 
         long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        System.out.println("Tempo de execução: " + executionTime + " milissegundos");
+        System.out.println("Tempo de execução: " + (endTime - startTime) + " milissegundos");
     }
 }
 
